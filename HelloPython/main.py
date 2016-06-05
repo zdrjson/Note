cc6ca3256a940eaec7377de39047d9a14f0682c4
张德荣 <zdrjson@gmail.com>
2016-06-05 13:19:53 +0800

    add -1 list test

--- HelloPython/main.py
+++ HelloPython/main.py
@@ -9,3 +9,17 @@ print(chr(25991))
 x = b'ABC'
 print('Hello, %s' % 'world')
 print('Hi, %s you have $%d.' % ('Michale', 100000))
+print('%2d-%02d' % (3,1))
+print('%.2f' % 3.1415926)
+print('Age: %s. Gender: %s' % (25, True))
+print('growth rate: %d %%' % 7)
+s = 'Python-中文'
+print(s)
+b = s.encode('utf-8')
+print(b)
+print(b.decode('utf-8'))
+classmethod = ['Michael', 'Bob', 'Tracy']
+print(classmethod)
+print(len(classmethod))
+print(classmethod[0])
+print(classmethod[-1])
