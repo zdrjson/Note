0a895ee73adc2866a8d9fda48de946e82b67116a
张德荣 <zdrjson@gmail.com>
2016-06-05 14:06:52 +0800

    test python loop

--- HelloPython/main.py
+++ HelloPython/main.py
@@ -117,5 +117,36 @@ else:
     print('kid')
 
 
+names = ['Michael', 'Bob', 'Tracy']
+for name in names:
+    print(name)
 
 
+sum = 0
+for x in [1,2,3,4,5,6,7,8,9,10]:
+    sum = sum + x
+
+print(sum)
+
+
+print(list(range(5)))
+
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum)
+
+sum = 0
+n = 99
+while n > 0:
+    sum = sum + n
+    n = n - 2
+print(sum)
+
+
+for i in ['Bart','Lisa', 'Adam']:
+    print('Hello, %s!' % i)
+
+
+# while 1:
+    # print(9)
