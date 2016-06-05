6a101ebec324cfb377e649c60a7085d655ad5614
张德荣 <zdrjson@gmail.com>
2016-06-05 13:51:21 +0800

    add python input test

--- HelloPython/main.py
+++ HelloPython/main.py
@@ -47,4 +47,75 @@ print(classmethod)
 t = (1)
 print(t)
 
+t = (1,)
+print(t)
+
 # 定义的不是tuple，是1这个数！这是因为括号()既可以表示tuple，又可以表示数学公式中的小括号，这就产生了歧义，因此，Python规定，这种情况下，按小括号进行计算，计算结果自然是1。
+t = ('a','b', ['A','B'])
+t[2][1] = 'X'
+t[2][1] = 'Y'
+print(t)
+
+L = [
+    ['Apple','Google', 'Microsoft'],
+    ['Java', 'Python','Ruby','PHP',
+     ['Adam','Bart','Lisa']]
+]
+
+print(L[0][0])
+
+
+age = 20
+if age >= 18:
+    print('your age is', age)
+    print('adult')
+else:
+    print('your age is', age)
+    print('teenager')
+
+
+age = 3
+if age >= 18:
+    print('adult')
+elif age >= 6:
+    print('teenager')
+else:
+    print('kid')
+
+
+age = 20
+if age >= 6:
+    print('teenager')
+elif age >= 18:
+    print('adult')
+else:
+    print('kid')
+
+# 只要x是非零数值、非空字符串、非空list等，就判断为True，否则为False
+if x:
+    print('True')
+
+
+s = input('birth: ')
+birth = int(s)
+if birth < 2000:
+    print('00前')
+else:
+    print('00后')
+
+# 注意:
+# input()返回的是字符串
+# 必须通过int()将字符串转换为整数
+# 才能用于数值比较:
+
+age = int(input('Input your age: '))
+if age >= 18:
+    print('adult')
+elif age >= 6:
+    print('teenager')
+else:
+    print('kid')
+
+
+
+
