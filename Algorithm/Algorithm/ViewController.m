d34840e39e3bada7e1fa1a39ba9833d2bc42c1bd
张德荣 <zdrjson@gmail.com>
2016-06-11 16:26:34 +0800

    add binary search description

--- Algorithm/Algorithm/ViewController.m
+++ Algorithm/Algorithm/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    int array[] = {0, 1, 3, 4, 5, 6, 7, 8, 9, 10};
+    int array[] = {0, 1,2, 3, 4, 5, 6, 7, 8, 9, 10};
     int count = sizeof(array) / sizeof(array[0]);
     int target = 5;
     int start = 0, end = count - 1, mid =0;
@@ -36,22 +36,23 @@
     } else {
         printf("not found\n");
     }
-   NSLog(@"%d",binarySearch1(array, 0, 9, 5));
+   NSLog(@"%d",binarySearch1(array, 0, 9, 2));
 }
+//二分查找又称折半查找，优点是比较次数少，查找速度快，平均性能好；其缺点是要求待查表为有序表，且插入删除困难。因此，折半查找方法适用于不经常变动而查找频繁的有序列表。
 //递归方法
-int binarySearch1(int a[] , int low , int high , int findNum)
-{
-    int mid = ( low + high ) / 2;
-    if (low > high)
+int binarySearch1(int a[], int low, int high, int findNum) {
+    int mid = (low + high) / 2;
+    if (low > high) {
         return -1;
-    else
-    {
-        if (a[mid] > findNum)
-            return binarySearch1(a, low, mid - 1, findNum);
-        else if (a[mid] < findNum)
-            return binarySearch1(a, mid + 1, high, findNum);
-        else
+    }
+    else{
+        if (a[mid] > findNum) {
+            return binarySearch1(a,low,high - 1,findNum);
+        } else if (a[mid] < findNum){
+            return binarySearch1(a,low +1, high, findNum);
+        } else {
             return mid;
+        }
     }
 }
 //非递归方法
