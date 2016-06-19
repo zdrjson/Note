c963f96a9993110b74e6472199cb1c5ab5681a53
张德荣 <zdrjson@gmail.com>
2016-06-19 22:12:05 +0800

    test py mutable argument

--- python/LearnPython/flow.py
+++ python/LearnPython/flow.py
@@ -21,3 +21,39 @@ def add_end(L=None):
 print(add_end())
 print(add_end())
 
+def calc(numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+
+
+print(calc([1,2,3]))
+print(calc([1,3,5,7]))
+
+
+def calc(*numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+
+print(calc(1,2))
+print(calc())
+
+nums = [1,2,3]
+print(calc(nums[0],nums[1],nums[2]))
+# *nums表示把nums这个list的所有元素作为可变参数传进去。这种写法相当有用，而且很常见。
+print(calc(*nums))
+
+
+
+
+
+
+
+
+
+
+
+
