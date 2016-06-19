03905bf40cdad3829b288e38e74722c45c1d455e
张德荣 <zdrjson@gmail.com>
2016-06-20 07:14:04 +0800

    test py Slice

--- python/LearnPython/flow.py
+++ python/LearnPython/flow.py
@@ -99,5 +99,47 @@ print(f2(*args, **kw))
 
 
 
+def fact(n):
+    if n==1:
+        return 1
+    return n * fact(n-1)
+
+print(fact(1))
+
+print(fact(5))
+
+print(fact(100))
 
 
+# 尾递归是指，在函数返回的时候，调用自身本身，并且，return语句不能包含表达式。这样，编译器或者解释器就可以把尾递归做优化，使递归本身无论调用多少次，都只占用一个栈帧，不会出现栈溢出的情况。
+
+def fact(n):
+    return fact_iter(n,1)
+
+def fact_iter(num, product):
+    if num == 1:
+        return  product
+    return fact_iter(num - 1,num *product)
+
+print(fact(5))
+
+
+
+L = ['Michael','Sarah','Tracy','Bob','Jack']
+
+
+print(L[:3])
+print(L[-2:])
+print(L[-2:-1])
+
+
+L = list(range(100))
+
+print(L[:10])
+
+print(L[-10:])
+print(L[10:20])
+
+print(L[:10:2])
+
+print(L[::5])
