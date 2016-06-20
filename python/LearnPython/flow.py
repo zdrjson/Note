336013746650b1ed12189478216a7f8ac26a5dac
张德荣 <zdrjson@gmail.com>
2016-06-20 09:56:28 +0800

    test py generator

--- python/LearnPython/flow.py
+++ python/LearnPython/flow.py
@@ -178,6 +178,35 @@ for i, value in enumerate(['A','B','C']):
 for x, y in [(1,1), (2,4), (3,9)]:
     print(x, y)
 
+print([x * x for x in range(1,11)])
+
+
+print([x * x for x in range(1,11) if x % 2 == 0])
+print([m + n for m in 'ABC' for n in 'XYZ'])
+
+
+import os
+
+print([d for d in os.listdir('.')])
+d = {'x':'A','y':'B','z':'C'}
+for k, v in d.items():
+    print(k, '=',v)
+
+print([k + '=' + v for k, v in d.items()])
+
+
+L = ['Hello','World','IBM','Apple']
+print([s.lower() for s in L])
+
+
+
+L = [x * x for x in range(10)]
+print(L)
+g = (x * x for x in range(10))
+print(g)
+
+
+
 
 
 
