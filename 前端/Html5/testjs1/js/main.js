737c05491dddffc9030d7cfd7c4734085a60d9f2
张德荣 <zdrjson@gmail.com>
2016-06-26 08:50:43 +0800

    add jquery to study

---
+++ 前端/Html5/testjs1/js/main.js
@@ -0,0 +1,24 @@
+
+var ps = $('p');
+ps.length;
+
+var a = $('.red');
+var a = $('.red.green');
+
+var email = $('[name=email]');
+
+var icon = $('[name^=icon]');
+var names =$('[name$=with]');
+
+
+var icons = $('[class^="icon-"]');
+
+var eamilInput = $('input[name=email]');
+
+var tr = $('tr.red');
+
+$('p,div');
+$('p.red,p.green');
+
+
+
