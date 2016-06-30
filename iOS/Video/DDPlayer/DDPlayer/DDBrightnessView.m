dcec3ab3a2a96e9b0fab798bad0f80c386339b4d
张德荣 <zdrjson@gmail.com>
2016-06-30 09:24:35 +0800

    add onevat.js test

--- iOS/Video/DDPlayer/DDPlayer/DDBrightnessView.m
+++ iOS/Video/DDPlayer/DDPlayer/DDBrightnessView.m
@@ -8,6 +8,16 @@
 
 #import "DDBrightnessView.h"
 #import "DDPlayer.h"
+
+@interface DDBrightnessView ()
+@property (nonatomic, strong) UIImageView *backImage;
+@property (nonatomic, strong) UILabel *title;
+@property (nonatomic, strong) UIView *longView;
+@property (nonatomic, strong) NSMutableArray *tipArray;
+@property (nonatomic, assign) BOOL orientationDidChange;
+@property (nonatomic, strong) NSTimer *timer;
+@end
+
 @implementation DDBrightnessView
 
 + (instancetype)sharedInstance
@@ -37,10 +47,42 @@
         toolbar.alpha = 0.97;
         [self addSubview:toolbar];
         
+        self.title = ({
+            UILabel *label = [[UILabel alloc] init];
+            label.font = [UIFont boldSystemFontOfSize:16];
+            [UIColor colorWithRed:0.25f green:0.22f blue:0.21f alpha:1.00f];
+            
+            label.textAlignment = NSTextAlignmentCenter;
+            label.text = @"高度";
+            [self addSubview:label];
+            label;
+        });
         
+        self.longView = ({
+            UIView *view = [[UIView alloc] init];
+            view.backgroundColor = [UIColor colorWithRed:0.25f green:0.22f blue:0.21f alpha:1.00f];
+            [self addSubview:view];
+            view;
+        });
+
+        [self createTips];
+
         
     }
     return self;
+}
+// 创建Tips
+- (void)createTips {
+    self.tipArray = [NSMutableArray arrayWithCapacity:16];
+    
+    CGFloat tipW = (self.longView.bounds.size.width - 17) / 16;
+    CGFloat tipH = 5;
+    CGFloat tipY = 1;
+    
+    for (int i = 0; i<16; i++) {
+        CGFloat tipX = i * (tipW + 1) + 1;
+        
+    }
 }
 
 @end
