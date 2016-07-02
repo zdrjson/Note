daffb533ffe6c511b5319435d8f1a531f4847556
张德荣 <zdrjson@gmail.com>
2016-07-02 10:41:11 +0800

    test python Iterator and Iterable

--- python/LearnPython/algorithm.py
+++ python/LearnPython/algorithm.py
@@ -1,3 +1,23 @@
+from collections import Iterable, Iterator
+print(isinstance([],Iterable))
+print(isinstance({},Iterable))
+print(isinstance('abc',Iterable))
+print(isinstance((x for x in range(10)),Iterable))
+print(isinstance(100,Iterable))
+
+print(isinstance((x for x in range(10)), Iterator))
+print(isinstance([],Iterator))
+
+print(isinstance({},Iterator))
+print(isinstance('abc',Iterator))
+
+
+print(isinstance(iter([]), Iterator))
+
+
+print(isinstance(iter('abc'), Iterator))
+
+
 
 def removeElement(self, A, elem):
     ret = 0
@@ -7,3 +27,13 @@ def removeElement(self, A, elem):
             ret += 1
 
     return ret
+
+L = [x * x for x in range(10)]
+print(L);
+g = (x * x for x in range(10))
+print(g)
+for n in g:
+    print(n)
+
+
+
