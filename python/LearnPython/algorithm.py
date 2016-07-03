211cb9f83da9a635e1ad94ff6c8da10d9b284cc9
张德荣 <zdrjson@gmail.com>
2016-07-03 14:20:42 +0800

    test python 偏函数

--- python/LearnPython/algorithm.py
+++ python/LearnPython/algorithm.py
@@ -264,6 +264,39 @@ def log(text):
     return decorator
 
 
+print(int('12345', base=8))
+print(int('12345', 16))
+
+def int2(x, base=2):
+    return int(x, base)
+
+print(int2('1000000'))
+print(int2('1010101'))
+
+
+import functools
+int2 = functools.partial(int, base=2)
+print(int2('1000000'))
+print(int2('1010101'))
+
+print(int2('1000000', base=10))
+
+
+int2 = functools.partial(int, base=2)
+int2('10010')
+# 相当于
+kw = {'base':2}
+int('10010',**kw)
+
+max2 = functools.partial(max, 10)
+
+max2(5,6,7)
+# 相当于
+args = (10,5,6,7)
+max(*args)
+
+
+
 
 
 
