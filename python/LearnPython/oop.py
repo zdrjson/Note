6a458d08668135b4ebad48d16df0ccb0ec2f738f
张德荣 <zdrjson@gmail.com>
2016-07-04 23:57:26 +0800

    test py class

--- python/LearnPython/oop.py
+++ python/LearnPython/oop.py
@@ -11,3 +11,24 @@ bart = Student('Bart Simpson', 59)
 lisa = Student('Lisa Simpson', 87)
 bart.print_score()
 lisa.print_score()
+
+class Student(object):
+    pass
+bart = Student()
+
+bart.name = 'Bart Simpson'
+print(bart.name)
+
+
+
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name;
+        self.score = score
+
+bart = Student('Bart Simpson', 59)
+print(bart.name)
+
+print(bart.score)
+
+
