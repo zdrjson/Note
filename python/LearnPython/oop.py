28acad4a72a2bcb55d59ccad91677666ade393fa
张德荣 <zdrjson@gmail.com>
2016-07-06 23:27:33 +0800

    add py 访问限制

--- python/LearnPython/oop.py
+++ python/LearnPython/oop.py
@@ -48,3 +48,31 @@ bart.print_score()
 
 print(bart.get_grade())
 
+
+
+
+class Student(object):
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+
+    def print_score(self):
+        print('%s: %s' % (self.__name, self.__score))
+
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+
+    def set_socre(self, score):
+        if 0 <= score <= 100:
+            self.__score = score
+        else:
+            raise  ValueError('bad score')
+
+
+bart = Student('Bart Simpson', 98)
+
+print(bart._Student__name)
+
