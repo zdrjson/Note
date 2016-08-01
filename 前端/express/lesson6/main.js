78c5002f02a8f4992a5ac4fe753148bd9f1edfe6
张德荣 <zdrjson@gmail.com>
2016-08-01 11:57:54 +0800

    add ruanyifengjs.html

--- 前端/express/lesson6/main.js
+++ 前端/express/lesson6/main.js
@@ -1,4 +1,13 @@
 var fibonacci = function (n) {
+	if (typeof n !== 'number') {
+		throw new Error('n should be a Number');
+	}
+	if (n < 0) {
+		throw new Error('n should >= 0')
+	}
+	if (n > 10) {
+		throw new Error('n should <= 10')
+	}
 	if (n === 0) {
 		return 0;
 	}
@@ -7,6 +16,8 @@ var fibonacci = function (n) {
 	}
 	return fibonacci(n-1) + fibonacci(n-2);
 };
+
+exports.fibonacci = fibonacci;
 
 if (require.main === module) {
 	//如果是直接执行main.js 则进入此处
