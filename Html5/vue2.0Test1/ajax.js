ffd3d4813731f81e88b1f0a3f44b84851b55b6e4
json <zdrjson@gmail.com>
2016-10-12 14:03:16 +0800

    test ruanyifeng ajax

---
+++ Html5/vue2.0Test1/ajax.js
@@ -0,0 +1,24 @@
+/**
+ * Created by json on 16/10/11.
+ */
+
+var ajax = new XMLHttpRequest();
+ajax.open('GET','https://www.baidu.com',true);
+
+ajax.onreadystatechange = handleStateChange;
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+          console.log(xhr.responseText);
+      } else  {
+          console.error(xhr.statusText);
+      }
+  }
+};
+
+xhr.onerror = function (e) {
+    console.error(xhr.statusText);
+}
+
