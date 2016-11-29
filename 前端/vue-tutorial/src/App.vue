2328707df8419576d6adda900b01a4139c4aa18b
j <j@SG01.local>
2016-11-29 16:00:08 +0800

    add vue-tutorial for learing vue2.0

---
+++ 前端/vue-tutorial/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <!--<div id="app">-->
+    <!--<img src="./assets/logo.png">-->
+    <!--<hello></hello>-->
+  <!--</div>-->
+  <div id="wrapper">
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <a>
+           <i class="glyphicon glyhicon-time">
+             计划板
+           </i>
+        </a>
+        <ul class="nav navbar-nav">
+          <li><router-link to="/home">首页</router-link></li>
+          <li><router-link to="/time-entries">计划列表</router-link></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="col-sm-3">
+
+      </div>
+      <div class="cols-sm-9">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 前端/vue-tutorial/src/components/Home.vue
@@ -0,0 +1,9 @@
+<template xmlns="http://www.w3.org/1999/XSL/Transform">
+  <div class="jumbotron">
+    <h1>任务追踪</h1>
+    <p></p>
+      <strong>
+        <router-link to="/time-entries">创建一个任务</router-link>
+      </strong>
+    </div>
+  </template>
