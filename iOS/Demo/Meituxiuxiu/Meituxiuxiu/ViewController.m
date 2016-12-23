695f064077eb9d5752fff0d7214d38766407cae5
J S <zdrjson@gmail.com>
2016-12-23 10:37:37 +0800

    addd the project of Meituxiuxiu

---
+++ iOS/Demo/Meituxiuxiu/Meituxiuxiu/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Meituxiuxiu
+//
+//  Created by J S on 12/23/16.
+//  Copyright © 2016 J S. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ iOS/Demo/Meituxiuxiu/Meituxiuxiu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Meituxiuxiu
+//
+//  Created by J S on 12/23/16.
+//  Copyright © 2016 J S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
