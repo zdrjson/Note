89bb4dffe40129ee598c98d10fdf0714c7f36bba
zhangderong1 <zhangderong1@jd.com>
2019-10-18 12:50:02 +0800

    add LongestContinuousIncreasingSubsequence.java

---
+++ Algorithm/LongestContinuousIncreasingSubsequence.java
@@ -0,0 +1,23 @@
+/*
+* Idea: Dynamic Propramming
+* Time Complexity : O(n)
+* Space Complexity: O(1)
+* */
+class Solution {
+	public int findLengthOfLCIS(int[] nums) {
+	    if (nums.length == 0) {
+	        return 0;
+        }
+	    int cur = 1;
+	    int ans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i-1]) {
+                cur++;
+                ans = Math.max(ans, cur);
+            } else  {
+                cur = 1;
+            }
+        }
+        return  ans;
+    }
+}
