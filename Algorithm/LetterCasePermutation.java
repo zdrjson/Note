3fe4bafaa3889f292afe7e071ae053867655acc8
zhangderong1 <zhangderong1@jd.com>
2019-12-05 01:23:19 +0800

    LetterCasePermutation.java

---
+++ Algorithm/LetterCasePermutation.java
@@ -0,0 +1,22 @@
+/* Time complexity : O(n*2^l), l= # of letters in the string
+   Space complexity: O(n) + O(n*2^l) 
+*/
+class Solution {
+    public List<String> letterCasePermutation(String S) {
+        List<String> ans = new ArrayList<>();
+        dfs(S.toCharArray(), 0, ans);
+        return ans;
+    }
+    
+    private void dfs(char[] S, int i, List<String> ans) {
+        if (i == S.length) {
+            ans.add(new String(S));
+            return;
+        }
+        dfs(S, i + 1, ans);
+        if (!Character.isLetter(S[i])) return;
+        S[i] ^= 1 << 5;
+        dfs(S, i + 1, ans);
+        S[i] ^= 1 << 5;
+    }
+}
