1603ca74917d6f57f02ad42808901a257d64728c
zhangderong1 <zhangderong1@jd.com>
2019-12-04 17:00:38 +0800

    refresh BinarySearch.java

--- Algorithm/BinarySearch.java
+++ Algorithm/BinarySearch.java
@@ -1,11 +1,11 @@
-/*Time complexity: (Ologn)
+/*Time complexity: O(logn)
   Space complexity : O(1)
 */
 class Solution {
     public int search(int[] nums, int target) {
         int l = 0;
         int r = nums.length;
-        while (l < r)  {
+        while (l < r) {
             int m = l + (r - l) / 2;
             if (nums[m] == target) {
                 return m;
