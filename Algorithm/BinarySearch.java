dd2b93f15f427806f8384b1df0541a5f8860ef21
zhangderong1 <zhangderong1@jd.com>
2019-12-04 16:55:52 +0800

    BinarySearch.java

---
+++ Algorithm/BinarySearch.java
@@ -0,0 +1,20 @@
+/*Time complexity: (Ologn)
+  Space complexity : O(1)
+*/
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length;
+        while (l < r)  {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] > target) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return -1;
+    }
+}
