325bbf387a554df0b7c2ccd3be4d1189a39d5891
zhangderong1 <zhangderong1@jd.com>
2019-12-07 23:29:40 +0800

    ConvertBSTtoGreaterTree.java

---
+++ Algorithm/ConvertBSTtoGreaterTree.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// Solution: reversed inorder traaversal
+// in a BST, we can visit every node in the decreasing order. Using a member sum to track the sum of all visited nodes.
+class Solution {
+    int sum;
+    public TreeNode convertBST(TreeNode root) {
+        sum = 0;
+        rinorder(root);
+        return root;
+    }
+    private void rinorder(TreeNode root) {
+        if (root == null) return;
+        rinorder(root.right);
+        root.val = (sum += root.val);
+        rinorder(root.left);    
+    }
+}
