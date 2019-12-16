40aa88879679769a731422d0abb8769a52b9385d
zhangderong1 <zhangderong1@jd.com>
2019-12-16 14:06:40 +0800

    PartitionLabels.java

---
+++ Algorithm/PartitionLabels.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> partitionLabels(String S) {
+        int lastIndex[] = new int[128];
+        for (int i = 0; i < S.length(); ++i) {
+            lastIndex[(int)S.charAt(i)] = i;
+        }
+        List<Integer> ans = new ArrayList<>();
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < S.length(); ++i) {
+            end = Math.max(end, lastIndex[(int)S.charAt(i)]);
+            if (i == end) {
+                ans.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return ans;
+    }
+}
