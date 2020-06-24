b823872822f7730a9f248453c95592ffb15dd640
zhangderong1 <zhangderong1@jd.com>
2020-06-24 11:52:15 +0800

    update MinimumCostForTickets.java

--- Algorithm/MinimumCostForTickets.java
+++ Algorithm/MinimumCostForTickets.java
@@ -1,10 +1,12 @@
+// Solution: DP
+// dp[i] := min cost to cover the i-th day
+// dp[0] = 0
+// dp[i] = min(dp[i - 1] + costs[0], dp[i - 7] + costs[1], dp[i - 30] + costs[2])
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
         int[] req = new int[days[days.length - 1] + 1];
         int[] dp = new int[days[days.length - 1] + 1];
-        for (int day : days) {
-            req[day] = 1;
-        }
+        for (int day : days) req[day] = 1;
         dp[0] = 0;
         for (int i = 1; i < dp.length; ++i) {
             if (req[i] != 1) {
