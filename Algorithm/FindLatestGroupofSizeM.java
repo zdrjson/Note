9b677216a9d13451ca91e8b3a6d772595bfb6702
zhangderong1 <zhangderong1@jd.com>
2020-08-28 04:58:13 +0800

    rename count to counts at FindLatestGroupofSizeM.java

--- Algorithm/FindLatestGroupofSizeM.java
+++ Algorithm/FindLatestGroupofSizeM.java
@@ -6,7 +6,7 @@ class Solution {
     public int findLatestStep(int[] arr, int m) {
         int n = arr.length;
         int[] len = new int[n + 2];
-        int[] count = new int[n + 2];
+        int[] counts = new int[n + 2];
         int ans = -1;
         for (int i = 0; i < n; ++i) {
             int x = arr[i];
@@ -14,10 +14,10 @@ class Solution {
             int r = len[x + 1];
             int t = 1 + l + r;
             len[x - l] = len[x + r] = t;
-            --count[l];
-            --count[r];
-            ++count[t];
-            if (count[m] != 0) ans = i + 1;
+            --counts[l];
+            --counts[r];
+            ++counts[t];
+            if (counts[m] != 0) ans = i + 1;
         }
         return ans;
     }
