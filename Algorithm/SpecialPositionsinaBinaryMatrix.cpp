e01749de77c497f3b5569ca1693daf6c6b368845
zhangderong1 <zhangderong1@jd.com>
2020-09-14 17:51:37 +0800

    add SpecialPositionsinaBinaryMatrix.cpp

---
+++ Algorithm/SpecialPositionsinaBinaryMatrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        const int rows = mat.size();
+        const int cols = mat[0].size();
+        vector<int> rs(rows);
+        vector<int> cs(cols);
+        for (int r = 0; r < rows; ++r)
+            for (int c = 0; c < cols; ++c) {
+                rs[r] += mat[r][c];
+                cs[c] += mat[r][c];
+            }
+        int ans = 0;
+        for (int r = 0; r < rows; ++r)
+            for (int c = 0; c < cols; ++c)
+                ans += mat[r][c] && rs[r] == 1 && cs[c] == 1;
+        return ans;
+    }
+};
