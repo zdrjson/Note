4969d4f2b7a51453c4ffcea87059af1d11fe6d9a
zhangderong1 <zhangderong1@jd.com>
2020-09-25 11:41:54 +0800

    fix the comment typo of MinimumCosttoConnectTwoGroupsofPoints.cpp

--- Algorithm/MinimumCosttoConnectTwoGroupsofPoints.cpp
+++ Algorithm/MinimumCosttoConnectTwoGroupsofPoints.cpp
@@ -5,7 +5,7 @@ public:
         const int m = cost.size();
         const int n = cost[0].size();
         // dp[i][s] := min cost to connect first i points in group1
-        // and points (bitmask s) int group2.
+        // and points (bitmask s) in group2.
         vector<vector<int>> dp(m + 1, vector<int>(1 << n, kInf));
         dp[0][0] = 0;
         for (int i = 0; i < m; ++i)
