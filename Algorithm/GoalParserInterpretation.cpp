378a980891bde506299204d6f98dfc6874c6a409
zhangderong1 <zhangderong1@jd.com>
2020-12-07 11:23:53 +0800

    add GoalParserInterpretation.cpp

---
+++ Algorithm/GoalParserInterpretation.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string ans;
+        for (int i = 0; i < command.size(); ++i) {
+            if (command[i] == 'G') ans += "G";
+            else if (command[i] == '(') {
+                if (command[i + 1] == ')') ans += "o";
+                else ans += "al";
+            }
+        }
+        return ans;
+    }
+};
