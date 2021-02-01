3620845c357e856e1110367fe02df224d4588b89
zhangderong1 <zhangderong1@jd.com>
2021-02-01 20:25:22 +0800

    add BinaryTreeMaximumPathSum.cpp

---
+++ Algorithm/BinaryTreeMaximumPathSum.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// Solution: Recursion
+// Time complexity: O(n)
+// Space complexity: O(h)
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        if (!root) return 0;
+        int ans = INT_MIN;
+        maxPathSum(root, ans);
+        return ans;
+    }
+private:
+    int maxPathSum(TreeNode* root, int& ans) {
+        if (!root) return 0;
+        int l = max(0, maxPathSum(root->left, ans));
+        int r = max(0, maxPathSum(root->right, ans));
+        int sum = l + r + root->val;
+        ans = max(ans, sum);
+        return max(l, r) + root->val;
+    }
+};
