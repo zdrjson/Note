2e688e9a5b4e464637d57041303f53b8cdc839be
zhangderong1 <zhangderong1@jd.com>
2021-06-07 13:53:35 +0800

    modified FindLatestGroupofSizeM.java

--- Algorithm/FindLatestGroupofSizeM.java
+++ Algorithm/FindLatestGroupofSizeM.java
@@ -4,16 +4,16 @@
 // Space complexity: O(n)
 class Solution {
     public int findLatestStep(int[] arr, int m) {
-        int n = arr.length;
-        int[] len = new int[n + 2];
+        final int n = arr.length;
+        int[] lens = new int[n + 2];
         int[] counts = new int[n + 2];
         int ans = -1;
         for (int i = 0; i < n; ++i) {
-            int x = arr[i];
-            int l = len[x - 1];
-            int r = len[x + 1];
-            int t = 1 + l + r;
-            len[x - l] = len[x + r] = t;
+            final int x = arr[i];
+            final int l = lens[x - 1];
+            final int r = lens[x + 1];
+            final int t = 1 + l + r;
+            lens[x - l] = lens[x + r] = t;
             --counts[l];
             --counts[r];
             ++counts[t];
