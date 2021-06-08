982eadc45a3dc986491ace95318599ad2b8aeee6
zhangderong1 <zhangderong1@jd.com>
2021-06-08 08:59:47 +0800

    add RangeAdditionII.cpp

---
+++ Algorithm/RangeAdditionII.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        for (const auto& range : ops) {
+            m = min(m, range[0]);
+            n = min(n, range[1]);
+        }
+        return m * n;
+    }
+};
