10c64c8199e20c87a96cdbb5acb1bde65023dda9
zhangderong1 <zhangderong1@jd.com>
2021-06-09 11:39:32 +0800

    add SubdomainVisitCount.java

---
+++ Algorithm/SubdomainVisitCount.java
@@ -0,0 +1,23 @@
+// Solution: HashTable
+// Time complexity: O(n)
+// Space complexity: O(n)
+class Solution {
+    public List<String> subdomainVisits(String[] cpdomains) {
+        Map<String, Integer> counts = new HashMap<>();
+        for (final String cpdomain : cpdomains) {
+            int index = cpdomain.indexOf(" ");
+            int count = Integer.valueOf(cpdomain.substring(0, index));
+            String domain = cpdomain.substring(index + 1);
+            while (true) {
+                counts.put(domain, counts.getOrDefault(domain, 0) + count);
+                int i = domain.indexOf(".");
+                if (i == -1) break;
+                domain = domain.substring(i + 1);
+            }
+        }
+        List<String> ans = new ArrayList<>();
+        for (final Map.Entry<String, Integer> kv : counts.entrySet())  // 不要忘了Map.Entry前面加上final。
+            ans.add(kv.getValue() + " " + kv.getKey());
+        return ans; 
+    }
+}
