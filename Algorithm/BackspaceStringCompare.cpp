98a204832fa6c9548db7860cfaa1718cd9008677
zhangderong1 <zhangderong1@jd.com>
2021-06-10 16:03:13 +0800

    add BackspaceStringCompare.cpp

---
+++ Algorithm/BackspaceStringCompare.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool backspaceCompare(string S, string T) {
+        return type(S) == type(T);
+    }
+private:
+    string type(string S) {
+        string o;
+        for (char c : S) 
+            if (c == '#') {
+                if (!o.empty())
+                o.pop_back();
+            } else {
+                o.push_back(c);
+            }
+        return o;
+    }
+};
