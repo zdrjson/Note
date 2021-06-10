93916b517412bb826b2b59c88db62434ff3ea298
zhangderong1 <zhangderong1@jd.com>
2021-06-10 16:02:59 +0800

    add BackspaceStringCompare.java

---
+++ Algorithm/BackspaceStringCompare.java
@@ -0,0 +1,20 @@
+// Solution: Simulation
+// Time complexity: O(|S| + |T|)
+// Space complexity: O(|S| + |T|)
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        return type(S).equals(type(T));
+    }
+    private String type(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == '#') {
+                if (sb.length() > 0)
+                    sb.setLength(sb.length() - 1);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
