3c8172c403a648d1bdf88e1f2358ce49e056e9df
zhangderong1 <zhangderong1@jd.com>
2021-06-12 09:55:20 +0800

    add ToLowerCase.cpp

---
+++ Algorithm/ToLowerCase.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for (char &c : str)
+            if (c >= 'A' && c <= 'Z') c = c - 'A' + 'a';
+        return str;
+    }
+};
