02db1f38ff3ae11e9193f76b1a02f42573eda919
zhangderong1 <zhangderong1@jd.com>
2021-06-18 22:51:11 +0800

    add CustomSortString.java

---
+++ English/CustomSortString.java
@@ -0,0 +1,24 @@
+// Solution 1: HashTable + Sorting
+// 1. Store the order of char in a hashtable
+// 2. Sort the string based on the order
+// Time complexity: O(nlogn)
+// Space complexity: O(128)
+class Solution {
+    public String customSortString(String S, String T) {
+        int[] orders = new int[128];
+        Arrays.fill(orders, Integer.MAX_VALUE);
+        int i = 0;
+        for (char c : S.toCharArray())
+            if (orders[c] == Integer.MAX_VALUE) orders[c] = ++i;
+        Character[] ans = new Character[T.length()];
+        for (int j = 0; j < ans.length; ++j)
+            ans[j] = new Character(T.charAt(j));
+        Arrays.sort(ans, (c1, c2) -> {
+           return orders[c1] - orders[c2]; 
+        });
+        char[] res = new char[ans.length];
+        for (int k = 0; k < res.length; ++k)
+            res[k] = ans[k];
+        return new String(res); // 或者用 String.valueOf(res) 都可以，但是不能用String.valueOf(ans)。ans为Character[]类型，答案不对, 直接new String(ans)不能编译。
+    }
+}
