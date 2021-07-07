3e83db218f2de67d8f225251e58bb1e93efa24eb
zhangderong1 <zhangderong1@jd.com>
2021-07-07 16:29:46 +0800

    add LongestSubstringWithoutRepeatingCharacters.java

--- Algorithm/LongestSubstringWithoutRepeatingCharacters.java
+++ Algorithm/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,3 +1,8 @@
+// Using a hashtable to remember the last index of every char. And keep tracking the starting point of a valid substring.
+// start = max(start, last[s[i]] + 1)
+// ans = max(ans, i - start + 1)
+// Time complexity: O(n)
+// Space complexity: O(128)
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int[] last = new int[128];
@@ -5,9 +10,8 @@ class Solution {
         int start = 0;
         int ans = 0;
         for (int i = 0; i < s.length(); ++i) {
-            if (last[s.charAt(i)] != -1) {
+            if (last[s.charAt(i)] != -1) 
                 start = Math.max(start, last[s.charAt(i)] + 1);
-            }
             last[s.charAt(i)] = i;
             ans = Math.max(ans, i - start + 1);
         }
